e83c53e8bd69cbbaa0d032a16df8438855d12c18
jomsystem-sam <sam@jomsystem.com>
2023-09-01 16:45:43 +0800

    Company Profile Edit & Delete Function
    Added Edit and delete function for company profile, added option to redirect to company profile in nav bar.

--- JS.AMSWeb/Areas/CompanyModule/CompanyProfileController.cs
+++ JS.AMSWeb/Areas/CompanyModule/CompanyProfileController.cs
@@ -77,286 +77,100 @@ namespace JS.AMSWeb.Areas.CompanyModule
             return RedirectToAction("Index");
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Create(AddCompanyProfileViewModel dto)
-        //{
-        //    try
-        //    {
-        //        var user = await _db.Users.SingleOrDefaultAsync(m => m.UserName == User.Identity.Name);
-        //        if (user == null)
-        //        {
-        //            return BadRequest("Current user not found");
-        //        }
 
-        //        var state = _db.States
-        //            .FirstOrDefault(x => x.Id == dto.StateId);
-        //        if (state == null)
-        //        {
-        //            return BadRequest("State not found");
-        //        }
 
-        //        var city = _db.Cities
-        //            .FirstOrDefault(x => x.Id == dto.CityId);
-        //        if (city == null)
-        //        {
-        //            return BadRequest("City not found");
-        //        }
+        public IActionResult Edit(Guid id)
+        {
+            var companyProfile = _db.CompanyProfiles
+                .FirstOrDefault(x => x.Id == id);
+            if (companyProfile == null)
+            {
+                return BadRequest("Company profile not found");
+            }
 
-        //        var location = new Location();
-        //        location.Name = dto.CompanyName;
-        //        location.State = state;
-        //        location.City = city;
-        //        location.AddressOne = dto.Address1;
-        //        location.AddressTwo = dto.Address2;
-        //        location.AddressThree = dto.Address3;
-        //        location.AddressFour = dto.Address4;
-        //        location.PostalCode = dto.Postcode;
+            var vm = new ManageCompanyProfileViewModel();
+            vm.Id = companyProfile.Id;
+            vm.Name = companyProfile.Name;
+            vm.BRN = companyProfile.BRN;
+            vm.FullAddress = companyProfile.FullAddress;
+            vm.ContactPersonName = companyProfile.ContactPersonName;
+            vm.ContactPersonPhoneNumber = companyProfile.ContactPersonPhoneNumber;
 
-        //        _db.Locations.Add(location);
-        //        _db.SaveChanges(User.Identity.Name);
+            return View(vm);
+        }
 
-        //        var companyProfile = new CompanyProfile();
-        //        companyProfile.Active = true;
-        //        companyProfile.CompanyName = dto.CompanyName;
-        //        companyProfile.InternalCompanyCode = dto.InternalCompanyCode;
-        //        companyProfile.PhoneNumber1 = dto.PhoneNumber1;
-        //        companyProfile.PhoneNumber2 = dto.PhoneNumber2;
-        //        companyProfile.EmailAddress = dto.EmailAddress;
-        //        companyProfile.BusinessRegistrationNumber = dto.BusinessRegistrationNumber;
-        //        companyProfile.Location = location;
+        [HttpPost]
+        public async Task<IActionResult> Edit(ManageCompanyProfileViewModel dto)
+        {
+            try
+            {
+                var companyProfile = _db.CompanyProfiles
+                    .FirstOrDefault(x => x.Id == dto.Id);
+                if (companyProfile == null)
+                {
+                    return BadRequest("Company profile not found");
+                }
 
-        //        _db.CompanyProfiles.Add(companyProfile);
-        //        _db.SaveChanges(User.Identity.Name);
+                companyProfile.Name = dto.Name;
+                companyProfile.BRN = dto.BRN;
+                companyProfile.FullAddress = dto.FullAddress;
+                companyProfile.ContactPersonName = dto.ContactPersonName;
+                companyProfile.ContactPersonPhoneNumber = dto.ContactPersonPhoneNumber;
 
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
+                _db.CompanyProfiles.Update(companyProfile);
+                await _db.SaveChangesAsync("system");
 
-        //public IActionResult Edit(Guid id)
-        //{
-        //    var companyProfile = _db.CompanyProfiles
-        //        .Include(m => m.CompanyDocumentSettings).ThenInclude(m => m.DocumentType)
-        //        .FirstOrDefault(x => x.Id == id);
-        //    if (companyProfile == null)
-        //    {
-        //        return BadRequest("Company profile not found");
-        //    }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-        //    var vm = new ManageCompanyProfileViewModel();
-        //    vm.Id = companyProfile.Id;
-        //    vm.CompanyName = companyProfile.CompanyName;
-        //    vm.InternalCompanyCode = companyProfile.InternalCompanyCode;
-        //    vm.PhoneNumber1 = companyProfile.PhoneNumber1 ?? "-";
-        //    vm.PhoneNumber2 = companyProfile.PhoneNumber2;
-        //    vm.LocationId = companyProfile.LocationId;
-        //    vm.Address1 = companyProfile.Location?.AddressOne ?? "";
-        //    vm.Address2 = companyProfile.Location?.AddressTwo ?? "";
-        //    vm.Address3 = companyProfile.Location?.AddressThree ?? "";
-        //    vm.Address4 = companyProfile.Location?.AddressFour ?? "";
-        //    vm.Postcode = companyProfile.Location?.PostalCode;
-        //    vm.StateId = companyProfile.Location?.StateId;
-        //    vm.CityId = companyProfile.Location?.CityId;
-        //    vm.EmailAddress = companyProfile.EmailAddress;
-        //    vm.BusinessRegistrationNumber = companyProfile.BusinessRegistrationNumber;
-        //    vm.DisplayAddress = companyProfile.Location != null ? Utils.CommonHelper.CombineLocationToFullAddress(companyProfile.Location) : "-";
+        public IActionResult Delete(Guid id)
+        {
+            var companyProfile = _db.CompanyProfiles
+                .FirstOrDefault(x => x.Id == id);
+            if (companyProfile == null)
+            {
+                return BadRequest("Company profile not found");
+            }
 
-        //    foreach (var companyBranch in companyProfile.CompanyBranches)
-        //    {
-        //        var cb = new CompanyBranchViewModel();
-        //        cb.BranchName = companyBranch.BranchName;
-        //        cb.BranchCode = companyBranch.BranchCode;
-        //        cb.DisplayAddress = companyBranch.Location != null ? Utils.CommonHelper.CombineLocationToFullAddress(companyBranch.Location) : "-";
-        //        cb.Id = companyBranch.Id;
+            var vm = new ManageCompanyProfileViewModel();
+            vm.Id = companyProfile.Id;
+            vm.Name = companyProfile.Name;
+            vm.BRN = companyProfile.BRN;
+            vm.FullAddress = companyProfile.FullAddress;
+            vm.ContactPersonName = companyProfile.ContactPersonName;
+            vm.ContactPersonPhoneNumber = companyProfile.ContactPersonPhoneNumber;
 
-        //        vm.CompanyBranches.Add(cb);
-        //    }
+            return View(vm);
+        }
 
-        //    foreach (var staff in companyProfile.CompanyStaffs)
-        //    {
-        //        var s = new StaffViewModel();
-        //        s.Id = staff.Id;
-        //        s.FullName = staff.FullName;
-        //        s.EmployeeId = staff.EmployeeId;
-        //        s.PhoneNumber = staff.PhoneNumber;
+        [HttpPost]
+        public async Task<IActionResult> Delete(ManageCompanyProfileViewModel dto)
+        {
+            try
+            {
+                var companyProfile = _db.CompanyProfiles
+                    .FirstOrDefault(x => x.Id == dto.Id);
+                if (companyProfile == null)
+                {
+                    return BadRequest("Company profile not found");
+                }
 
-        //        vm.Staffs.Add(s);
-        //    }
+                companyProfile.Active = false;
 
-        //    foreach (var companyDocumentType in companyProfile.CompanyDocumentSettings.Where(x => x.Active))
-        //    {
-        //        var cdt = new CompanyDocumentTypeViewModel();
-        //        cdt.DocumentType = companyDocumentType.DocumentType.Name;
-        //        cdt.Prefix = companyDocumentType.Prefix;
-        //        cdt.CurrentDocumentNo = companyDocumentType.CurrentDocumentNo;
-        //        cdt.NextDocumentNo = companyDocumentType.NextDocumentNo;
-        //        cdt.Id = companyDocumentType.Id;
+                _db.CompanyProfiles.Update(companyProfile);
+                await _db.SaveChangesAsync("system");
 
-        //        vm.CompanyDocumentTypes.Add(cdt);
-        //    }
-
-        //    ViewBag.States = new SelectList(_db.States.Where(x => x.Active), "Id", "Name");
-        //    ViewBag.Cities = new SelectList(_db.Cities.Where(x => x.Active), "Id", "Name");
-
-        //    return View(vm);
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> Edit(ManageCompanyProfileViewModel dto)
-        //{
-        //    try
-        //    {
-        //        var user = await _db.Users.SingleOrDefaultAsync(m => m.UserName == User.Identity.Name);
-        //        if (user == null)
-        //        {
-        //            return BadRequest("Current user not found");
-        //        }
-
-        //        var state = _db.States
-        //            .FirstOrDefault(x => x.Id == dto.StateId);
-        //        if (state == null)
-        //        {
-        //            return BadRequest("State not found");
-        //        }
-
-        //        var city = _db.Cities
-        //            .FirstOrDefault(x => x.Id == dto.CityId);
-        //        if (city == null)
-        //        {
-        //            return BadRequest("City not found");
-        //        }
-
-        //        var location = _db.Locations
-        //            .FirstOrDefault(x => x.Id == dto.LocationId);
-        //        if (location == null && dto.LocationId.HasValue)
-        //        {
-        //            return BadRequest("Location not found");
-        //        }
-
-        //        if (location != null && dto.LocationId.HasValue)
-        //        {
-        //            location.Name = dto.CompanyName;
-        //            location.State = state;
-        //            location.City = city;
-        //            location.AddressOne = dto.Address1;
-        //            location.AddressTwo = dto.Address2;
-        //            location.AddressThree = dto.Address3;
-        //            location.AddressFour = dto.Address4;
-        //            location.PostalCode = dto.Postcode;
-
-        //            _db.Locations.Update(location);
-        //            _db.SaveChanges(User.Identity.Name);
-        //        }
-
-        //        if (location == null && !dto.LocationId.HasValue)
-        //        {
-        //            var newLocation = new Location();
-        //            newLocation.Name = dto.CompanyName;
-        //            newLocation.State = state;
-        //            newLocation.City = city;
-        //            newLocation.AddressOne = dto.Address1;
-        //            newLocation.AddressTwo = dto.Address2;
-        //            newLocation.AddressThree = dto.Address3;
-        //            newLocation.AddressFour = dto.Address4;
-        //            newLocation.PostalCode = dto.Postcode;
-
-        //            _db.Locations.Add(newLocation);
-        //            _db.SaveChanges(User.Identity.Name);
-
-        //            location = newLocation;
-        //        }
-
-        //        var companyProfile = _db.CompanyProfiles
-        //            .FirstOrDefault(x => x.Id == dto.Id);
-        //        if (companyProfile == null)
-        //        {
-        //            return BadRequest("Company profile not found");
-        //        }
-
-        //        companyProfile.CompanyName = dto.CompanyName;
-        //        companyProfile.InternalCompanyCode = dto.InternalCompanyCode;
-        //        companyProfile.PhoneNumber1 = dto.PhoneNumber1;
-        //        companyProfile.PhoneNumber2 = dto.PhoneNumber2;
-        //        companyProfile.EmailAddress = dto.EmailAddress;
-        //        companyProfile.BusinessRegistrationNumber = dto.BusinessRegistrationNumber;
-        //        companyProfile.LocationId = location?.Id ?? null;
-
-        //        _db.CompanyProfiles.Update(companyProfile);
-        //        await _db.SaveChangesAsync(User.Identity.Name);
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        //public IActionResult Delete(Guid id)
-        //{
-        //    var companyProfile = _db.CompanyProfiles
-        //        .Include(m => m.Location)
-        //        .Include(m => m.Location).ThenInclude(m => m.City)
-        //        .Include(m => m.Location).ThenInclude(m => m.State)
-        //        .FirstOrDefault(x => x.Id == id);
-        //    if (companyProfile == null)
-        //    {
-        //        return BadRequest("Company profile not found");
-        //    }
-
-        //    var vm = new ManageCompanyProfileViewModel();
-        //    vm.CompanyName = companyProfile.CompanyName;
-        //    vm.InternalCompanyCode = companyProfile.InternalCompanyCode;
-        //    vm.PhoneNumber1 = companyProfile.PhoneNumber1;
-        //    vm.PhoneNumber2 = companyProfile.PhoneNumber2;
-        //    vm.EmailAddress = companyProfile.EmailAddress;
-        //    vm.BusinessRegistrationNumber = companyProfile.BusinessRegistrationNumber;
-        //    vm.LocationId = companyProfile.LocationId;
-        //    vm.Address1 = companyProfile.Location?.AddressOne ?? "";
-        //    vm.Address2 = companyProfile.Location?.AddressTwo ?? "";
-        //    vm.Address3 = companyProfile.Location?.AddressThree ?? "";
-        //    vm.Address4 = companyProfile.Location?.AddressFour ?? "";
-        //    vm.Postcode = companyProfile.Location?.PostalCode;
-        //    vm.StateId = companyProfile.Location?.StateId;
-        //    vm.CityId = companyProfile.Location?.CityId;
-        //    vm.CityName = companyProfile.Location.City.Name;
-        //    vm.StateName = companyProfile.Location.State.Name;
-
-        //    return View(vm);
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> Delete(ManageCompanyProfileViewModel dto)
-        //{
-        //    try
-        //    {
-        //        var user = await _db.Users.SingleOrDefaultAsync(m => m.UserName == User.Identity.Name);
-        //        if (user == null)
-        //        {
-        //            return BadRequest("Current user not found");
-        //        }
-
-        //        var companyProfile = _db.CompanyProfiles
-        //            .FirstOrDefault(x => x.Id == dto.Id);
-        //        if (companyProfile == null)
-        //        {
-        //            return BadRequest("Company profile not found");
-        //        }
-
-        //        companyProfile.Active = false;
-
-        //        _db.CompanyProfiles.Update(companyProfile);
-        //        await _db.SaveChangesAsync(User.Identity.Name);
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
