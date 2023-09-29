6cd5ee3cafbcd12543aa3b86f04fca1b9005d391
teejinlim <teejin@jomsystem.com>
2023-09-29 17:51:59 +0800

    Add Ticket History Media CRUD

---
+++ JS.AMSWeb/Areas/TicketModule/ViewModels/TicketHistoryMedia/AddTicketHistoryMediaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JS.AMS.Data.Entity.AssetModule;
+using JS.AMS.Data.Entity.CompanyModule;
+using JS.AMS.Data.Entity.TicketModule;
+
+namespace JS.AMSWeb.Areas.TicketModule.ViewModels.TicketHistoryMedia
+{
+	public class AddTicketHistoryMediaViewModel
+    {
+        public Guid TicketHistoryId { get; set; }
+        public TicketHistory TicketHistory { get; set; }
+
+        public Guid MediaId { get; set; }
+        //public Media Media { get; set; }
+
+        public string? Remark { get; set; }
+
+    }
+}
+
